2505e3afb3485cd076bf418505fcc03daee6fab0
Amir Batyrov <batyrov.amir@outlook.com>
2025-01-09 12:44:55 +0000

    Sign in  / Sign up route

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,55 +5,52 @@ import bcrypt from "bcrypt";
 const router = express.Router();
 
 //Create new User
-router.post("/signup", (req, res) => {
-  (async function () {
-    try {
-      const user = await User.findOne({
-        $or: [{ _id: req.body.username }, { email: req.body.email }],
+router.post("/signup", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      $or: [{ _id: req.body.username }, { email: req.body.email }],
+    });
+    if (user) {
+      res.status(409).json({ message: "User already exists" });
+    } else if (!user) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      const newUser = new User({
+        _id: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
       });
-      if (user) {
-        res.status(409).json({ message: "User already exists" });
-      } else if (!user) {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const newUser = new User({
-          _id: req.body.username,
-          email: req.body.email,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        res.status(201).json({ message: "User Created!" });
-      }
-    } catch (err) {
-      throw err;
+      await newUser.save();
+      res.status(201).json({ message: "User Created!" });
     }
-  })();
+  } catch (err) {
+    res.status(500);
+    throw err;
+  }
 });
 
 //signin
-router.post("/signin", (req, res) => {
-  (async function () {
-    try {
-      const user = await User.findOne({
-        $and: [{ email: req.body.email }, { password: req.body.email }],
-      });
-      if (user) {
-        res.status(409).json({ message: "User already exists" });
-      } else if (!user) {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const newUser = new User({
-          _id: req.body.username,
-          email: req.body.email,
-          password: hashedPassword,
-        });
-        await newUser.save();
-        res.status(201).json({ message: "User Created!" });
+router.post("/signin", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    if (user) {
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (match) {
+        res
+          .status(200)
+          .json({ message: "Sign In succesfull", username: user._id });
+      } else if (!match) {
+        res.status(401).json({ message: "Wrong email or password" });
       }
-    } catch (err) {
-      res.status(500)
+    } else if (!user) {
+      res.status(401).json({ message: "Wrong email or password" });
     }
-  })();
+  } catch (err) {
+    res.status(500);
+    throw err;
+  }
 });
 
 export default router;
