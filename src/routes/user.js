c68ac52cc6d34784eeb0aabadadd0e8c4356220e
Amir Batyrov <batyrov.amir@outlook.com>
2025-01-09 15:40:21 +0000

    done with signin/sigup

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,17 @@
+//Get Env Variables
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import User from "../../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+function generateAccessToken(email) {
+  return jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1800s",
+  });
+}
 
 const router = express.Router();
 
@@ -8,7 +19,7 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   try {
     const user = await User.findOne({
-      $or: [{ _id: req.body.username }, { email: req.body.email }],
+      $or: [{ username: req.body.username }, { email: req.body.email }],
     });
     if (user) {
       res.status(409).json({ message: "User already exists" });
@@ -16,7 +27,7 @@ router.post("/signup", async (req, res) => {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
       const newUser = new User({
-        _id: req.body.username,
+        username: req.body.username,
         email: req.body.email,
         password: hashedPassword,
       });
@@ -38,14 +49,13 @@ router.post("/signin", async (req, res) => {
     if (user) {
       const match = await bcrypt.compare(req.body.password, user.password);
       if (match) {
-        res
-          .status(200)
-          .json({ message: "Sign In succesfull", username: user._id });
+        const accessToken = generateAccessToken({ email: req.body.email });
+        res.status(200).json({ accessToken: accessToken });
       } else if (!match) {
-        res.status(401).json({ message: "Wrong email or password" });
+        res.status(401).json({ accessToken: false });
       }
     } else if (!user) {
-      res.status(401).json({ message: "Wrong email or password" });
+      res.status(401).json({ accessToken: false });
     }
   } catch (err) {
     res.status(500);
@@ -57,7 +67,7 @@ router.post("/signin", async (req, res) => {
 router.post("/username", async (req, res) => {
   try {
     const user = await User.findOne({
-      _id: req.body.username,
+      username: req.body.username,
     });
     if (user) {
       res.status(200).json({ exists: true });
